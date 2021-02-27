33a7f9a40a34dfc33eddbb5cebbbe63691a2168f
Luís Amorim <luishads@gmail.com>
2021-02-27 02:11:07 -0300

    Two-fer exercise

---
+++ two-fer/src/main/kotlin/TwoFer.kt
@@ -0,0 +1,8 @@
+internal fun twofer(name: String): String {
+    if (name.isEmpty()) {
+        return "One for you, one for me."
+    } else {
+            return "One for $name, one for me."
+    }
+}
+
